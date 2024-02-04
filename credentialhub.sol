6c3fb68ef6f25d530d8fead103b6f12eef86fba2
Md. Rayhan Islam Sefat <112571481+RayhanSefat@users.noreply.github.com>
2024-02-04 22:37:25 +0600

    first contract added

---
+++ credentialhub.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.8.0;
+
+import "./ownable.sol";
+import "./safemath.sol";
+
+contract CredentialHub is Ownable {
+
+    using SafeMath for uint256;
+    using SafeMath32 for uint32;
+    using SafeMath16 for uint16;
+
+    
+
+}
